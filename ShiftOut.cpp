060dbe47fc9562845aab816c858ae6f2f91c4586
Luis F Cevallos <luisfcevallos@gmail.com>
2020-02-29 22:08:04 -0800

    Using the functions shiftRegisterSetup() and shiftRegisterSend().

--- ShiftOut.cpp
+++ ShiftOut.cpp
@@ -9,32 +9,24 @@
 void shiftRegisterSetup(int, int, int);
 void shiftRegisterSend(byte);
 
-void setup()
-{
-    pinMode(LATCH_PIN, OUTPUT);
-    pinMode(DATA_PIN, OUTPUT);
-    pinMode(CLOCK_PIN, OUTPUT);
+void setup() {
+    shiftRegisterSetup(LATCH_PIN, DATA_PIN, CLOCK_PIN);
 }
 
-void loop()
-{
+void loop() {
     for (size_t counter = 0; counter <= 8; counter++) {
         int temp = BIT_SEED >> (8 - counter);
-
-        digitalWrite(LATCH_PIN, LOW);
-        shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, temp); // 13.66uS per bit 73.206Kbps
-        digitalWrite(LATCH_PIN, HIGH);
+        shiftRegisterSend(temp);
     }
 }
 
 void shiftRegisterSetup(int _latchPin, int _dataPin, int _clockPin) {
-    pinMode(LATCH_PIN, OUTPUT);
-    pinMode(DATA_PIN, OUTPUT);
-    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(_latchPin, OUTPUT);
+    pinMode(_dataPin, OUTPUT);
+    pinMode(_clockPin, OUTPUT);
 }
 
-void shiftRegisterSend(byte data)
-{
+void shiftRegisterSend(byte data) {
    digitalWrite(LATCH_PIN, LOW);
    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, data); // 15.775uS per bit 63.391Kbps
    digitalWrite(LATCH_PIN, HIGH);
