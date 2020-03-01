47b890482d76d9a073ebcdc9bee358f4b10ada13
Luis F Cevallos <luisfcevallos@gmail.com>
2020-03-01 11:11:16 -0800

    Taking out the code that is the example of Shifting a bit 1 from LSB to MSB. So leaving the library with just the function members.

--- ShiftOut.cpp
+++ ShiftOut.cpp
@@ -1,24 +1,4 @@
-#include <Arduino.h>
 #include "ShiftOut.h"
-
-#define LATCH_PIN 4
-#define DATA_PIN 5
-#define CLOCK_PIN 6
-
-#define BIT_SEED 128
-
-ShiftOut myRegister(LATCH_PIN, DATA_PIN, CLOCK_PIN);
-
-void setup() {
-    myRegister.init();
-}
-
-void loop() {
-    for (size_t counter = 0; counter <= 8; counter++) {
-        int temp = BIT_SEED >> (8 - counter);   // 0, 1, 2, 4, 8, 16, 32, 64, 128
-        myRegister.send(temp);
-    }
-}
 
 ShiftOut::ShiftOut(int _latchPin, int _dataPin, int _clockPin): latchPin(_latchPin),
     dataPin(_dataPin), clockPin(_clockPin) {}
